4a01053cf62b455f527ad550444eed6270438575
Simon Scholz <simon.scholz@vogella.com>
2018-11-23 16:08:03 +0100

    Added additional routes for testing

--- telegram-bot/src/main/java/com/simonscholz/bot/weather/config/TelegramRouter.java
+++ telegram-bot/src/main/java/com/simonscholz/bot/weather/config/TelegramRouter.java
@@ -1,6 +1,7 @@
 package com.simonscholz.bot.weather.config;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
@@ -8,10 +9,14 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicate;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.simonscholz.bot.weather.data.DmiCityRepository;
+import com.simonscholz.bot.weather.domain.DmiCity;
 import com.simonscholz.bot.weather.service.TelegramBotService;
 
 import de.simonscholz.telegram.bot.api.domain.Update;
@@ -19,10 +24,14 @@ import de.simonscholz.telegram.bot.api.domain.Update;
 @Configuration
 public class TelegramRouter {
 	@Bean
-	public RouterFunction<ServerResponse> route(TelegramBotService botService) {
-		return RouterFunctions.route(
-				POST("/telegram/webhook").and(accept(MediaType.APPLICATION_JSON))
+	public RouterFunction<ServerResponse> route(TelegramBotService botService, DmiCityRepository dmiCityRepository) {
+		return RouterFunctions
+				.route(POST("/telegram/webhook").and(accept(MediaType.APPLICATION_JSON))
 						.and(contentType(MediaType.APPLICATION_JSON)),
-				request -> ok().build(botService.webhook(request.bodyToMono(Update.class))));
+						request -> ok().build(botService.webhook(request.bodyToMono(Update.class))))
+				.andRoute(GET("/query"), req -> ok().body(dmiCityRepository.findAll(), DmiCity.class))
+				.andRoute(GET("/query/{city}"),
+						req -> ok().body(dmiCityRepository.findByLabelContainingIgnoreCase(req.pathVariable("city")),
+								DmiCity.class));
 	}
 }
