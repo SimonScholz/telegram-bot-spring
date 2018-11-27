b72b50ce592b4fe1fc67ea76ff6bd8a7a25b100a
Simon Scholz <simon.scholz@vogella.com>
2018-11-28 00:47:57 +0100

    Organize imports

--- src/main/java/com/simonscholz/bot/weather/config/TelegramRouter.java
+++ src/main/java/com/simonscholz/bot/weather/config/TelegramRouter.java
@@ -1,7 +1,7 @@
 package com.simonscholz.bot.weather.config;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
@@ -9,8 +9,6 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RequestPredicate;
-import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
